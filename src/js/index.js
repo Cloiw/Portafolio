1c28feb703b02b68e573908f6fe54645ca1bb7d2
Cloiw <sircli@hotmail.com>
2019-08-03 22:50:14 -0400

    probando navbar responsive

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
 //
+const navIcon = document.getElementById("nav-menu-icon")
+navIcon.addEventListener('click', () =>{
+    console.log("hola")
+    let navBar = document.getElementById("nav-ul-id");
+    if (navBar.className === "nav-ul") {
+      navBar.className += "-resp";
+    } else {
+      navBar.className = "nav-ul";
+    }
+  }
+)
