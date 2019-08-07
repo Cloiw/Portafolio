40d20f6737a2eb7635bd10c3e88e3ea336c76597
Cloiw <sircli@hotmail.com>
2019-08-07 12:10:49 -0400

    arreglando errores en transicion

--- src/js/index.js
+++ src/js/index.js
@@ -2,58 +2,71 @@
 const navIcon = document.getElementById("nav-menu-icon");
 const navBar = document.getElementById("nav-ul-id");
 
+
 const createNavBarLi = (text,href,navBar) =>{
-  let li = document.createElement("li");
-  let a = document.createElement("a");
+  const li = document.createElement("li");
+  const a = document.createElement("a");
+
   a.appendChild(document.createTextNode(text));
   a.setAttribute("href" , href);
   li.appendChild(a)
-  
   li.setAttribute("class", "nav-li");
   navBar.appendChild(li);
-  setTimeout(function() {
+
+  setTimeout( ()=> {
     li.className = li.className + " show";
   }, 100);
 }
 
-navIcon.addEventListener('click', () =>{
-    
+const hideNavBarLi = () =>{
+  for (i = 0; i < navBar.getElementsByTagName("LI").length; i++) {
+    navBar.getElementsByTagName("LI")[i].addEventListener('click', () =>{
+        navBar.className = navBar.className + " hidden";
+      
+        setTimeout( () => {
+          navBar.className = "nav-ul";
+        }, 500);
+    })
+  }
+}
 
-    if (navBar.className === "nav-ul") {
-      while (navBar.firstChild) {
-        navBar.firstChild.remove();
-      }
-      navBar.className += "-resp"; 
-      setTimeout(function() {
-        createNavBarLi("PROYECTOS","#/",navBar);
-      }, 0);
-      setTimeout(function() {
-        createNavBarLi("HERRAMIENTAS","#/",navBar);
-      }, 100);
-      setTimeout(function() {
-        createNavBarLi("ACERCA DE MI","#/",navBar);
-      }, 200);
-      setTimeout(function() {
-        createNavBarLi("CONTACTO","#/",navBar);
-      }, 300);
-      
-     
-      
-    } else {
-      navBar.className = "nav-ul";
+
+navIcon.addEventListener('click', () =>{
+
+  if (navBar.className === "nav-ul") {
+
+    while (navBar.firstChild) {
+      navBar.firstChild.remove();
     }
 
+    navBar.className += "-resp"; 
 
+    setTimeout(function() {
+      createNavBarLi("PROYECTOS","#",navBar);
+    }, 0);
 
+    setTimeout(function() {
+      createNavBarLi("HERRAMIENTAS","#/",navBar);
+    }, 100);
 
-for (i = 0; i < navBar.getElementsByTagName("LI").length; i++) {
-    navBar.getElementsByTagName("LI")[i].addEventListener('click', () =>{
+    setTimeout(function() {
+      createNavBarLi("ACERCA DE MI","#/",navBar);
+    }, 200);
+
+    setTimeout(function() {
+      createNavBarLi("CONTACTO","#/",navBar);
+    }, 300);
+    
+    setTimeout(function() {
+      hideNavBarLi();
+    }, 300);
       
+    } else {
+
+      navBar.className = navBar.className + " hidden";
       setTimeout(function () {
         navBar.className = "nav-ul";
-      }, 200);
-      
-    })
-}
+      }, 500);
+    }
   }
 )
